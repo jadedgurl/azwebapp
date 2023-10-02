d84d95ec2ce13ea9d5321ec38ab45a36f5f74954
jadedgurl <126923594+jadedgurl@users.noreply.github.com>
2023-10-02 18:43:08 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 locals{ 
-  storage_name= ["north","south","east","west"]
-  clusters_name= ["jade","saj","idk","douglas","emannuel","olarewaju"]
+ # storage_name= ["north","south","east","west"]
+  # clusters_name= ["jade","saj","idk","douglas","emannuel","olarewaju"]
   appgt_name= ["meme","pewds","jay","markiplier"]
   vm_name= ["lol","idk","whatsup"]
    
