5061c12656ff2eb55963a8ddf4fec740b8df1dce
jadedgurl <126923594+jadedgurl@users.noreply.github.com>
2023-10-02 19:01:03 -0400

    Update waf.tf

--- waf.tf
+++ waf.tf
@@ -1,92 +1,94 @@
-resource "azurerm_web_application_firewall_policy" "wafpol" {
-  name                = "mcit-wafpolicy"
+resource "azurerm_subnet" "frontend" {
+  name                 = "frontend"
+  resource_group_name  = azurerm_resource_group.butterfly.name
+  virtual_network_name = azurerm_virtual_network.mcitnet.name
+  address_prefixes     = ["10.254.0.0/24"]
+}
+
+resource "azurerm_subnet" "backend" {
+  name                 = "backend"
+  resource_group_name  = azurerm_resource_group.butterfly.name
+  virtual_network_name = azurerm_virtual_network.butterfly.name
+  address_prefixes     = ["10.254.2.0/24"]
+}
+
+resource "azurerm_public_ip" "mcitpip" {
+  name                = "mcit-pip"
   resource_group_name = azurerm_resource_group.butterfly.name
   location            = azurerm_resource_group.butterfly.location
+  allocation_method   = "Dynamic"
+}
 
-  custom_rules {
-    name      = "Rule1"
-    priority  = 1
-    rule_type = "MatchRule"
+# since these variables are re-used - a locals block makes this more maintainable
+locals {
+  backend_address_pool_name      = "${azurerm_virtual_network.mcitnet.name}-beap"
+  frontend_port_name             = "${azurerm_virtual_network.mcitnet.name}-feport"
+  frontend_ip_configuration_name = "${azurerm_virtual_network.mcitnet.name}-feip"
+  http_setting_name              = "${azurerm_virtual_network.mcitnet.name}-be-htst"
+  listener_name                  = "${azurerm_virtual_network.mcitnet.name}-httplstn"
+  request_routing_rule_name      = "${azurerm_virtual_network.mcitnet.name}-rqrt"
+  redirect_configuration_name    = "${azurerm_virtual_network.mcitnet.name}-rdrcfg"
+}
 
-    match_conditions {
-      match_variables {
-        variable_name = "RemoteAddr"
-      }
+resource "azurerm_application_gateway" "appgate" {
+  for_each            = azurerm_web_application_firewall_policy.mcit2023
+  name                = "${each.key}"
+  resource_group_name = azurerm_resource_group.butterfly.name
+  location            = azurerm_resource_group.butterfly.location
+  firewall_policy_id  = azurerm_web_application_firewall_policy.mcit2023.id
 
-      operator           = "IPMatch"
-      negation_condition = false
-      match_values       = ["192.168.1.0/24", "10.0.0.0/24"]
-    }
-
-    action = "Block"
+  sku {
+    name     = "Standard_v2"
+    tier     = "Standard_v2"
+    capacity = 2
   }
 
-  custom_rules {
-    name      = "Rule2"
-    priority  = 2
-    rule_type = "MatchRule"
-
-    match_conditions {
-      match_variables {
-        variable_name = "RemoteAddr"
-      }
-
-      operator           = "IPMatch"
-      negation_condition = false
-      match_values       = ["192.168.1.0/24"]
-    }
-
-    match_conditions {
-      match_variables {
-        variable_name = "RequestHeaders"
-        selector      = "UserAgent"
-      }
-
-      operator           = "Contains"
-      negation_condition = false
-      match_values       = ["Windows"]
-    }
-
-    action = "Block"
+  gateway_ip_configuration {
+    name      = "my-gateway-ip-configuration"
+    subnet_id = azurerm_subnet.frontend.id
   }
 
-  policy_settings {
-    enabled                     = true
-    mode                        = "Prevention"
-    request_body_check          = true
-    file_upload_limit_in_mb     = 100
-    max_request_body_size_in_kb = 128
+  frontend_port {
+    name = local.frontend_port_name
+    port = 80
   }
 
-  managed_rules {
-    exclusion {
-      match_variable          = "RequestHeaderNames"
-      selector                = "x-company-secret-header"
-      selector_match_operator = "Equals"
-    }
-    exclusion {
-      match_variable          = "RequestCookieNames"
-      selector                = "too-tasty"
-      selector_match_operator = "EndsWith"
-    }
+  frontend_ip_configuration {
+    name                 = local.frontend_ip_configuration_name
+    public_ip_address_id = azurerm_public_ip.mcitpip.id
+  }
 
-    managed_rule_set {
-      type    = "OWASP"
-      version = "3.2"
-      rule_group_override {
-        rule_group_name = "REQUEST-920-PROTOCOL-ENFORCEMENT"
-        rule {
-          id      = "920300"
-          enabled = true
-          action  = "Log"
-        }
+  backend_address_pool {
+    name = local.backend_address_pool_name
+  }
 
-        rule {
-          id      = "920440"
-          enabled = true
-          action  = "Block"
-        }
-      }
-    }
+  backend_http_settings {
+    name                  = local.http_setting_name
+    cookie_based_affinity = "Disabled"
+    path                  = "/path1/"
+    port                  = 80
+    protocol              = "Http"
+    request_timeout       = 60
+  }
+  waf_configuration{
+  enabled          = true
+  firewall_mode    = "Prevention"
+  rule_set_type    = "Microsoft_BotManagerRuleSet"
+  rule_set_version = 3.1
+  }
+  http_listener {
+    name                           = local.listener_name
+    frontend_ip_configuration_name = local.frontend_ip_configuration_name
+    frontend_port_name             = local.frontend_port_name
+    protocol                       = "Http"
+  }
+
+  request_routing_rule {
+    name                       = local.request_routing_rule_name
+    priority                   = 9
+    rule_type                  = "Basic"
+    http_listener_name         = local.listener_name
+    backend_address_pool_name  = local.backend_address_pool_name
+    backend_http_settings_name = local.http_setting_name
   }
 }
