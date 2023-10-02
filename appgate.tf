df952f66a7bcd1999f64c80f01ce6e82308d5a2b
jadedgurl <126923594+jadedgurl@users.noreply.github.com>
2023-10-02 18:41:55 -0400

    Update appgate.tf

--- appgate.tf
+++ appgate.tf
@@ -12,7 +12,7 @@ resource "azurerm_subnet" "backend" {
   address_prefixes     = ["10.254.2.0/24"]
 }
 
-resource "azurerm_public_ip" "mcitptpip" {
+resource "azurerm_public_ip" "mcitpip" {
   name                = "mcit-pip"
   resource_group_name = azurerm_resource_group.butterfly.name
   location            = azurerm_resource_group.butterfly.location
