19067b3e245d5c8f125967e1bcdbdf22a4dd4797
jadedgurl <126923594+jadedgurl@users.noreply.github.com>
2023-10-02 18:32:12 -0400

    Create vm.tf

---
+++ vm.tf
@@ -0,0 +1,64 @@
+resource "azurerm_virtual_network" "mcitnet" {
+  name                = "${var.prefix}-network"
+  address_space       = var.address_space
+  location            = azurerm_resource_group.butterfly.location
+  resource_group_name = azurerm_resource_group.butterfly.name
+}
+
+resource "azurerm_subnet" "internal" {
+  name                 = "internal"
+  resource_group_name  = azurerm_resource_group.butterfly.name
+  virtual_network_name = azurerm_virtual_network.mcitnet.name
+  address_prefixes     = ["10.0.2.0/24"]
+}
+
+resource "azurerm_network_interface" "main" {
+  name                = "${var.prefix}-nic"
+  location            = azurerm_resource_group.butterfly.location
+  resource_group_name = azurerm_resource_group.butterfly.name
+
+  ip_configuration {
+    name                          = "testconfiguration1"
+    subnet_id                     = azurerm_subnet.internal.id
+    private_ip_address_allocation = "Dynamic"
+  }
+}
+
+resource "azurerm_virtual_machine" "main" {
+  for_each              = {for vm in local.vm_name:vm=>vm}
+  name                  = "${var.prefix}-vm-${each.key}"
+  location              = azurerm_resource_group.butterfly.location
+  resource_group_name   = azurerm_resource_group.butterfly.name
+  network_interface_ids = [azurerm_network_interface.main.id]
+  vm_size               = "Standard_DS1_v2"
+
+  # Uncomment this line to delete the OS disk automatically when deleting the VM
+  # delete_os_disk_on_termination = true
+
+  # Uncomment this line to delete the data disks automatically when deleting the VM
+  # delete_data_disks_on_termination = true
+
+  storage_image_reference {
+    publisher = "Canonical"
+    offer     = "0001-com-ubuntu-server-focal"
+    sku       = "20_04-lts"
+    version   = "latest"
+  }
+  storage_os_disk {
+    name              = "myosdisk1"
+    caching           = "ReadWrite"
+    create_option     = "FromImage"
+    managed_disk_type = "Standard_LRS"
+  }
+  os_profile {
+    computer_name  = var.computer_name
+    admin_username = var.admin_username
+    admin_password = var.admin_password
+  }
+  os_profile_linux_config {
+    disable_password_authentication = false
+  }
+  tags = {
+    environment = "staging"
+  }
+}
