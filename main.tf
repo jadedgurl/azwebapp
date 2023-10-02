a2fb4ded3d824ec610a268507b9b804226303fdd
jadedgurl <126923594+jadedgurl@users.noreply.github.com>
2023-10-02 18:12:57 -0400

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,9 @@
+locals{ 
+  storage_name= ["north","south","east","west"]
+  clusters_name= ["jade","saj","idk","douglas","emannuel","olarewaju"]
+
+}
+  resource "azurerm_resource_group" "butterfly" {
+  name     = "lmao"
+  location = "Canada Central"
+}
