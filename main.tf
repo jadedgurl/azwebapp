599e4e3f7e0939d1ce74b1d301044bccb9ed0773
jadedgurl <126923594+jadedgurl@users.noreply.github.com>
2023-10-02 18:36:33 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,7 +1,9 @@
 locals{ 
   storage_name= ["north","south","east","west"]
   clusters_name= ["jade","saj","idk","douglas","emannuel","olarewaju"]
-
+  appgt_name= ["meme","pewds","jay","markiplier"]
+  vm_name= ["lol","idk","whatsup"]
+   
 }
   resource "azurerm_resource_group" "butterfly" {
   name     = "lmao"
