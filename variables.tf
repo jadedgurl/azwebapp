c6d8e186c09279f1d1259501eaf2cd9be329d937
jadedgurl <126923594+jadedgurl@users.noreply.github.com>
2023-10-02 19:13:10 -0400

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -24,24 +24,4 @@ variable "address_space"{
   type=list(string)
   default=["10.0.0.0/16","10.0.1.0/24"]
 }
-variable "computer_name"{
-  type=string
-}
-variable "admin_username"{
-  type=string
-}
-variable "admin_password"{
-  type=string
-}
-variable "administrator_login"{
-  type=string
-}
-variable "administrator_login_password"{
-  type=string
-}
-variable "administrator_login1"{
-  type=string
-}
-variable "administrator_login_password1"{
-  type=string
-}
+
