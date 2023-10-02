e9f1a1cbd6ead1fa160ccd331d330e000cc97cf6
jadedgurl <126923594+jadedgurl@users.noreply.github.com>
2023-10-02 19:52:05 -0400

    Update gateway.tf

--- gateway.tf
+++ gateway.tf
@@ -82,7 +82,7 @@ resource "azurerm_application_gateway" "appgate" {
     frontend_ip_configuration_name = local.frontend_ip_configuration_name
     frontend_port_name             = local.frontend_port_name
     protocol                       = "Http"
-    firewall_policy_id             = "${each.key}"
+    firewall_policy_id             = each.value.id
   }
 
   request_routing_rule {
